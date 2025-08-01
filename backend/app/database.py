abe4c75426fff8e3fda881e3524ad6bc7a4e0b64
Safwan Shiblee <shibleesafwan@gmail.com>
2025-08-01 12:04:57 -0400

    Supabase Attempt

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://elections_db_ckkt_user:isAgxHZ8JYKCyi8owYjR0ilygxi1u6fl@dpg-d203klmuk2gs738tqgng-a.virginia-postgres.render.com/elections_db_ckkt"
+DATABASE_URL = "postgresql://postgres.odntfvdstdolcnnuolvh:vIJmGlyFIxCWTqXF@aws-0-us-east-2.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
