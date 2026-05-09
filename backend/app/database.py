fdfe44adf4c7fead51b3b64bd48d85a28fb354b8
Safwan Shiblee <shibleesafwan@gmail.com>
2026-05-09 09:43:16 -0400

    Correct password

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres.krqkaipoaekwylisilwg:odntfvdstdolcnnuolvh@aws-1-us-west-1.pooler.supabase.com:5432/postgres"
+DATABASE_URL = "postgresql://postgres.krqkaipoaekwylisilwg:ozGqyLohLs4flisa@aws-1-us-west-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
